7e353bddad7acffb7fd0a92adf22cfca31cd56c6
jinjinddd <ksj2512632@naver.com>
2022-11-04 13:24:16 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="name">
+    {{name}}
+    </div> 
+    <button class="btn btn-primary"
+    v-on:click="updatename"
+    >
+    Click
+    </button>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {  
+  setup() {
+    const name = ref('seonjin')
+    // const greeting = (name) =>{
+    //   return "hello," + name;
+    // }
+    // const greet = greeting(name);
+    const updatename = () =>{
+      // console.log('hello world');
+      name.id =2;
+      console.log(name);
+    };
+
+     return{
+     name,
+     updatename
+    };
+  }
+}
+</script>
+
+<style>
+.name{
+  color:red;
+}
+</style>
