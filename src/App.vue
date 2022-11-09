5d53ef1a7b7978d007a090e7a46f2f01d5011bd4
jinjinddd <ksj2512632@naver.com>
2022-11-09 15:27:00 +0900

    emit

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,82 @@
 <template>
 <div class = "container">
   <h2> To - Do List </h2>
-  <form @submit.prevent="onSubmit">
-      <div class="d-flex">
-          <div class ="flex-grow-1  mr-2">
-              <input 
-                  class="form-control"
-                  type="text" 
-                  v-model="todo"
-                  placeholder="Type new to-do"
-              >
-            </div>
-              <div>
-                  <button class="btn btn-primary"
-                    type = "submit">
-                      Add
-                  </button>
-              </div>
-      </div>
-      <div v-show="hasError" style="color: red">
-        (error)글을 입력하세요.
-      </div>
-
-  </form>
+  <TodoSimpleForm @add-todo="addTodo"/>
+ 
   <!-- 배열에 있는 데이터들을 똑같은 템플릿을 적용해서 반복적으로 보여주고싶을 때  V-For사용 (KEY을 사용해야한다.)-->
+    <div v-if="!todolist.length">
+      추가된 일정이 없습니다.
+    </div>
+    
     <div 
-      v-for=" t in todolist" 
+      v-for=" (t, index) in todolist" 
       :key="t.id"
       class="card mt-3"
       >
-        <div class="card-body p-2 ">
-          <div class = "form-check">
+        <div class="card-body p-2 d-flex align-items-center">
+          <div class = "form-check flex-grow-1">
             <input 
               class = "form-check-input"
               type="checkbox"
               v-model="t.completed">
             <label 
                 class = "form-check-label" 
+                :class="{ todo:t.completed }"
                 > 
                 {{t.subject}}
               </label>
           </div>
-           
+           <div>
+            <button 
+              class="btn btn-danger btn-sm"
+              @click="deleteTodo(index)">
+              Delete
+              </button>
+           </div>
         </div>
-
     </div>
 </div>    
 </template>
 
 <script>
 import {ref} from 'vue';
-export default {  
-  setup() {
-    const hasError = ref(false);
-    const todo= ref('');
-    const todolist = ref([]);
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
-    const onSubmit = () =>{
-      if(todo.value == ''){
-        hasError.value = true;
-      }else{
-          todolist.value.push({
-            id: Date.now(),
-            subject: todo.value,
-            completed: false
-            });
-            
-            hasError.value = false;
-            todo.value=''; //입력 후 입력상자 빈칸으로
-      }
+export default {  
+  components: {
+    TodoSimpleForm 
+  },
+  setup() {
+    const todolist = ref([]);
+    const todoStyle = {
+      textDecoration: 'line-through',
+      color:'gray'
+
+    }
+
+    const addTodo = (todo) =>{
+      console.log(todo);
+      todolist.value.push(todo);
+
+      };
+
+      const deleteTodo = (index) =>{
+          todolist.value.splice(index,1); //splice메서드는 배열의 기존 요소를 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경
       };
 
      return{
-        todo,
         todolist,
-        onSubmit,
-        hasError
+        addTodo,
+        todoStyle,
+        deleteTodo
       };
   }
 }
 </script>
 
 <style>
-.name{
-  color:red;
+.todo{
+  color:gray;
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,56 @@
+<template>
+     <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+          <div class ="flex-grow-1  mr-2">
+              <input 
+                  class="form-control"
+                  type="text" 
+                  v-model="todo"
+                  placeholder="Type new to-do"
+              >
+            </div>
+              <div>
+                  <button class="btn btn-primary"
+                    type = "submit">
+                      Add
+                  </button>
+              </div>
+      </div>
+      <div v-show="hasError" style="color: red">
+        (Error)글을 입력하세요.
+      </div>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup(props, context) { //composition API를 사용할때 setup()메소드 안에 두개의 인자를 받아온다.                   
+        const hasError = ref(false); 
+        const todo= ref('');
+        
+        const onSubmit = () =>{
+        if(todo.value == ''){
+            hasError.value = true;
+         }else{
+            context.emit('add-todo',{
+                id: Date.now(),
+                subject: todo.value,
+                completed: false
+            });//자식컴포넌트에서 부모컴포넌트로 데이터를 보낼 때 필요한 기능이 context에 들어있다. emit을 통해서 데이터를 보낸다.               
+            hasError.value = false;
+            todo.value=''; //입력 후 입력상자 빈칸으로
+            }
+      };
+        return{
+            todo,
+            hasError,
+            onSubmit
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
