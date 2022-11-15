6ecb943f8e713e04f06a4ebdd337e6780949cd63
jinjinddd <ksj2512632@naver.com>
2022-11-15 15:27:23 +0900

    json-server pakage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div class = "container">
   <h2> To - Do List </h2>
+  <!-- 검색input -->
   <input 
     class="form-control"
     type="text" 
@@ -9,8 +10,9 @@
    >
    <hr />
   <TodoSimpleForm @add-todo="addTodo"/>
-    <div v-if="!todolist.length">
-      추가된 일정이 없습니다.
+  <div style="color: red">{{error}}</div>
+    <div v-if="!filteredTodos.length">
+     표시할 내용이 없습니다.
     </div>
     <TodoList 
     :todolist="filteredTodos"  
@@ -23,7 +25,8 @@
 <script>
 import {ref,computed} from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
-import TodoList from './components/TodoList.vue';
+import TodoList       from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {  
   components: {
@@ -35,13 +38,29 @@ export default {
     const todoStyle = {
       textDecoration: 'line-through',
       color:'gray'
-    }
-
+    };
+    const error=ref('');
     const addTodo = (todo) =>{
-      console.log(todo);
-      todolist.value.push(todo);
+        error.value=''; //error메시지 초기화
+        //데이터베이스에 todo를 저장한 후 배열에 저장을 한다.axios패키지를 이용(요청을 보낼 때 사용하는 패키지)
+        axios.post('http://localhost:3000/todolist',{
+        //id는 db에 넣지 않아도 자동으로 1부터 추가된다.
+        subject: todo.subject,
+        completed: todo.completed,
+          }).then(res => {
+          //자바스크립트에서 요청은 비동기적으로 일어난다. 응답이 promise란 곳으로오게되는데
+          //요청을 보내고 요청이 끝나고 응답이 오기전에 바로 다음으로 넘어가게된다.
+          //then 키워드를 통해 요청에 대한 응답이 왔을 때 실행할 수 있다.
+          //then이 없다면 요청이 끝났는지 아닌지 알 수 없을 때 다음 문장이 실행된다.
+              console.log(res);
+              todolist.value.push(res.data); //id까지 포함된 정보가 배열에 저장된다.
+             }).catch(err =>{ //데이터베이스에 저장실패했을 때 catch실행된다.
+              console.log(err);
+              error.value = '데이터베이스 에러발생~';
+             });     
       };
 
+   
       const toggleTodo = (index) =>{
         console.log(index)
         todolist.value[index].completed = !todolist.value[index].completed
@@ -70,7 +89,8 @@ export default {
         deleteTodo,
         toggleTodo,
         searchText,
-        filteredTodos
+        filteredTodos,
+        error,
       };
   }
 }
