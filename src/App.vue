3329fcc063b48829dd00046be13fb1af8beb1d3c
jinjinddd <ksj2512632@naver.com>
2022-11-15 14:07:39 +0900

    검색기능추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
 <div class = "container">
   <h2> To - Do List </h2>
+  <input 
+    class="form-control"
+    type="text" 
+    v-model="searchText"
+    placeholder="Search"
+   >
+   <hr />
   <TodoSimpleForm @add-todo="addTodo"/>
     <div v-if="!todolist.length">
       추가된 일정이 없습니다.
     </div>
-    <TodoList :todolist="todolist" @toggle-todo="toggleTodo"/>
+    <TodoList 
+    :todolist="filteredTodos"  
+    @toggle-todo="toggleTodo" 
+    @delete-todo="deleteTodo"
+    />
 </div>    
 </template>
 
 <script>
-import {ref} from 'vue';
+import {ref,computed} from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
 
@@ -24,7 +35,6 @@ export default {
     const todoStyle = {
       textDecoration: 'line-through',
       color:'gray'
-
     }
 
     const addTodo = (todo) =>{
@@ -34,18 +44,33 @@ export default {
 
       const toggleTodo = (index) =>{
         console.log(index)
+        todolist.value[index].completed = !todolist.value[index].completed
       };
 
       const deleteTodo = (index) =>{
-          todolist.value.splice(index,1); //splice메서드는 배열의 기존 요소를 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경
+          todolist.value.splice(index, 1); //splice메서드는 배열의 기존 요소를 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경
       };
+
+      //검색기능
+      const searchText = ref('');
+      const filteredTodos = computed(()=>{
+        if(searchText.value){
+          return todolist.value.filter(todo => {
+            return todo.subject.includes(searchText.value);
+          });
+        }
+        return todolist.value;
+      });
+
 
      return{
         todolist,
         addTodo,
         todoStyle,
         deleteTodo,
-        toggleTodo
+        toggleTodo,
+        searchText,
+        filteredTodos
       };
   }
 }
