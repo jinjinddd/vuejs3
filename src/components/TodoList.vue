0fa357e1ecd080746ef35f23b102dccd446bd0f0
jinjinddd <ksj2512632@naver.com>
2022-11-25 13:23:15 +0900

    css plus

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,51 +1,63 @@
-<template>
-       <!-- 배열에 있는 데이터들을 똑같은 템플릿을 적용해서 반복적으로 보여주고싶을 때  V-For사용 (KEY을 사용해야한다.)-->
-    <div 
-      v-for=" (t, index) in todolist" 
-      :key="t.id"
-      class="card mt-3"
-      >
+<!--Todo목록을 보는 페이지-->
+<template >
+  <!-- 배열에 있는 데이터들을 똑같은 템플릿을 적용해서 반복적으로 보여주고싶을 때  V-For사용 (KEY을 사용해야한다.)-->
+    <List
+      :items="todolist"
+      :index="index"
+    >
+    <template #default="{item, index}">
         <div 
         class="card-body p-2 d-flex align-items-center"
-        @click="moveToPage(t.id)"
+        @click="moveToPage(item.id)"
         style="cursor: pointer"
         >
-          <div class = "form-check flex-grow-1">
+          <div class = " flex-grow-1">
               <!--props는 자식컴포넌트가 받은 데이터를 직접적으로 변경할 수 없다 v-modle="t.completed"를 변경해주어야함-->
            <!--체크박스-->
             <input 
-              class = "form-check-input"
+              class="ml-2 mr-2"
               type="checkbox"
-              :checked="t.completed"
+              :checked="item.completed"
               @change="toggleTodo(index, $event)"
               @click.stop
               >
           
-            <label 
-                class = "form-check-label" 
-                :class="{ todo:t.completed }"
-                > 
-                {{t.subject}}
-              </label>
+            <span :class="{ todo:item.completed }"> 
+                {{item.subject}}
+              </span>
           </div>
            <div>
             <!--삭제버튼-->
-            <button 
-              class="btn btn-danger btn-sm"
-              @click.stop="deleteTodo(index)"
-              >   
-              Delete
-              </button>
+              <font-awesome-icon icon="fa-trash" class="remove-btn"  @click.stop="openModal(item.id)" />
            </div>
         </div>
-    </div>
+    </template>
+    </List>
+  
+
+     <teleport to="#modal">
+        <DeleteModal 
+          v-if ="showModal"
+          @close ="closeModal"
+          @delete ="deleteTodo"
+        />
+    </teleport>
+     
 </template>
 
 <script>
 import { useRouter } from 'vue-router';
+import DeleteModal from '@/components/DeleteModal.vue'
+import { ref } from 'vue';
+import List from '@/components/List.vue'
+
 
 //import { watchEffect } from 'vue';
 export default {
+  components:{
+    DeleteModal,
+    List
+  },
     //props: ['todolist']
     props: {
         todolist:{
@@ -57,13 +69,29 @@ export default {
 
 
     setup(props, {emit}){
+       const showModal = ref(false);
+       const todoDeleteId = ref(null);
+
       const router = useRouter();
         const toggleTodo = (index, event) =>{
             emit('toggle-todo', index, event.target.checked);
         };
 
-        const deleteTodo = (index) =>{
-          emit('delete-todo', index);
+        const openModal =(id)=>{
+          todoDeleteId.value = id;
+          showModal.value=true;
+        };
+
+        const closeModal = () =>{
+          todoDeleteId.value = null;
+          showModal.value= false;
+        };
+
+        const deleteTodo = () =>{
+          emit('delete-todo', todoDeleteId.value);
+
+          showModal.value = false;
+          todoDeleteId.value = null;
         };
          
 /*         watchEffect(()=>{
@@ -78,18 +106,42 @@ export default {
           })
         };
 
+       
+
         return{
             toggleTodo,
             deleteTodo,
             moveToPage,
+            showModal,
+            openModal,
+            closeModal,
+
         };
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.item {
+  background: white;
+  width: 500px;
+  margin: 5px;
+  padding: 15px 20px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  flex-basis: fit-content;
+  
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
 .todo{
   color:gray;
   text-decoration: line-through;
+}
+ .remove-btn {
+        margin-left: 10px;
+        color: #de4343;
+        cursor: pointer;
 }
 </style>
